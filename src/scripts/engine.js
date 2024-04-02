d0faba6a0df5a076cdadf24c084af4b3c8a995e5
dayvidtp <dayvid.pinto0@gmail.com>
2024-04-02 18:45:34 -0300

    set-Card-Fields

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -75,8 +75,24 @@ async function createCardImage(IdCard, fieldSide) {
 
     }
 
-
     return cardImage;
+}
+
+async function setCardsFieldcard(cardId) {
+    await removeAllCardsImages();
+    
+    let computerCardId = await getRandomCardId();
+
+    state.fieldCards.player.style.display = "block";
+    state.fieldCards.computer.style.display = "block";
+
+    state.fieldCards.player.src = cardData[cardId].img;
+    state.fieldCards.computer.src = cardData[computerCardId].img;
+
+    let duelResults = await checkDuelResults(cardId, computerCardId);
+
+    await updateScore();
+    await drawButton(duelResults);
 }
 
 async function drawSelectCard(index) {
