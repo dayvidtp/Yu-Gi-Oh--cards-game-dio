92b2e2ead9a75f3ee4eca477e3ab413e6633d083
dayvidtp <dayvid.pinto0@gmail.com>
2024-04-03 11:04:13 -0300

    final-settings

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -195,6 +195,9 @@ function init() {
 
     drawCards(5, playerSides.player1);
     drawCards(5, playerSides.computer);
+
+    const bgm = document.getElementById("bgm");
+    bgm.play();
 }
 
 init();
